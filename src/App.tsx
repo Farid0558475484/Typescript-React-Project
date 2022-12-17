e01f873473a5d1c4caef97a84b6cfd00d16257e1
Farid0558475484 <faridmh@code.edu.az>
2022-12-17 18:26:45 +0400

    type sum

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { type } from "@testing-library/user-event/dist/type";
 
 function App() {
+  // let a: number | string = 11;
+  // a = "ferid";
+
+  // let names: Array<string> = ["ferid", "ahmet", "mehmet", "ali", "veli"];
+
+  // alert(names[0].toUpperCase());
+
+  type AddressType = {
+    city?: string;
+    country?: string;
+    region: string;
+  };
+
+  type UserType = {
+    name: string;
+    age: number;
+    isMarried: boolean;
+    address: AddressType | null;
+  };
+
+  let user: UserType = {
+    name: "ferid",
+    age: 25,
+    isMarried: false,
+    address: {
+      city: "istanbul",
+      country: "turkey",
+      region: "europe",
+
+    },
+  };
+
+  const sum = (a: number, b: number)=> a+b;
+
+
+  // console.log(sum(1,2));
+  alert(sum(1,Number ("55")));
+  
+
   return (
     <div className="App">
       <header className="App-header">
